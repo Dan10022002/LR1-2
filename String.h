5e94f3dea6021e7213109ad2bf1498fb629fcf60
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2021-02-28 00:04:51 +0300

    Update String.h

--- String.h
+++ String.h
@@ -1,26 +1,27 @@
 #ifndef UNTITLED_STRING_H
 #define UNTITLED_STRING_H
-#include <fstream>
+#include <iostream>
 
 class String
 {
     char* element;
-    int string_size;
+    size_t string_size;
 public:
     String(); //1
     String(const String& object); //2
-    int Size(char* object); //7
-    explicit String(char* object); //3
+    size_t Size_help(const char* object); 
+    explicit String(const char* object); //3
     ~String(); //4
     String operator= (String& object); //5
     bool operator== (String& object); //6
+    size_t Size(); //7
     int Find(String& object); //8
     bool Find_help(String& object, int& h, int& time);
-    char operator[] (int& index); //10
+    char operator[] (int index); //10
     String operator+(String& object); //11
-    String Change(char& letter, char& needed_letter); //9
-    friend std::ostream& operator<<(std::ofstream& output, String& object); //12
-    friend std::istream& operator>>(std::ifstream& input, String& object); //12
+    String Change(char letter, char needed_letter); //9
+    friend std::ostream& operator<<(std::ostream& output, String& object); //12
+    friend std::istream& operator>>(std::istream& input, String& object); //12
 };
 
 #endif //UNTITLED_STRING_H
